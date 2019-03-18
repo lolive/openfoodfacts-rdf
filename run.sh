f226eae224981f3a5b492c65aa09e991e91d9aa1
vemonet <vincent.emonet@gmail.com>
2019-03-18 17:09:54 +0100

    fix issue with substitution and add dockerfile to easily execute the pipeline using Docker

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ which java > /dev/null || echo "Install Java first"
 which java > /dev/null || exit
 if [ ! -f sparql-generate-jena.jar ]; then
 	#Download a patched version of SparqlGenerate. Sources are available on GitHub at: https://github.com/lolive/sparql-generate/tree/master
-        curl --silent http://datao.net/sparql-generate-jena.jar > ./sparql-generate-jena.jar
+        curl --silent http://datao.net/sparql-generate-jena.jar > /app/sparql-generate-jena.jar
 fi
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 RDFDIR=$DIR/rdf
@@ -12,7 +12,7 @@ rm -Rf rdf
 mkdir rdf
 echo "Running... please wait! (3GB, and a lot of patience, are required :)"
 echo
-for i in *sg; do java -jar sparql-generate-jena.jar -q $i > rdf/${i/.sg/.rdf}& done
+for i in *sg; do java -jar /app/sparql-generate-jena.jar -q $i > rdf/${i}.rdf& done
 wait
 echo "RDF files are now available at directory " $RDFDIR
 cd - > /dev/null
