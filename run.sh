97c3f6ed516f61545e9e98e31fea2209a669b72f
lolive <github@lolive.net>
2019-03-18 09:34:08 +0100

    Jar will be checked and made available in the current directory instead of /tmp

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 which java > /dev/null || echo "Install Java first"
 which java > /dev/null || exit
-if [ ! -f /tmp/sparql-generate-jena.jar ]; then
+if [ ! -f sparql-generate-jena.jar ]; then
 	#Download a patched version of SparqlGenerate. Sources are available on GitHub at: https://github.com/lolive/sparql-generate/tree/master
-        curl --silent http://datao.net/sparql-generate-jena.jar > /tmp/sparql-generate-jena.jar
+        curl --silent http://datao.net/sparql-generate-jena.jar > ./sparql-generate-jena.jar
 fi
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 RDFDIR=$DIR/rdf
