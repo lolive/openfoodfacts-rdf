c9e94efc6e161ca7bedc7106ddf59483944b1f9f
vemonet <vincent.emonet@gmail.com>
2019-03-18 17:15:20 +0100

    remove the download of the jar from the run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 which java > /dev/null || echo "Install Java first"
 which java > /dev/null || exit
-if [ ! -f sparql-generate-jena.jar ]; then
-	#Download a patched version of SparqlGenerate. Sources are available on GitHub at: https://github.com/lolive/sparql-generate/tree/master
-        curl --silent http://datao.net/sparql-generate-jena.jar > /app/sparql-generate-jena.jar
-fi
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 RDFDIR=$DIR/rdf
 cd $DIR
